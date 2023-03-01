b2af7450314cae77909b0f8af77ef2632d49f838
explainthis.io <hello@explainthis.io>
2023-03-01 20:02:58 +0800

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+import os
+import requests
+from src.trading import BinanceTrading
+from src.logger import logger
+
+app = FastAPI()
+
+binance_trading = BinanceTrading(os.environ.get('API_KEY'), os.environ.get('API_SECRET_KEY'))
+
+
+@app.post('/webhook')
+async def tradingview_request(request: Request):
+    data = await request.json()
+    logger.info(data)
+    if data.get('passphrase', None) != os.environ.get('PASSPHRASE'):
+        logger.warning('Wrong passphrase')
+        return {
+            'code': 'error',
+            'message': 'Invalid passphrase'
+        }
+
+    symbol = data.get('symbol')
+    leverage = data.get('leverage')
+    quantity = data.get('quantity')
+    max_quantity_ratio = data.get('max_quantity_ratio')
+
+    if data.get('message') == 'Sell' or data.get('message') == 'Buy_Exit':
+        logger.info(f'SELL: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
+        binance_trading.sell(symbol, leverage, quantity, max_quantity_ratio)
+    elif data.get('message') == 'Buy' or data.get('message') == 'Sell_Exit':
+        logger.info(f'BUY: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
+        binance_trading.buy(symbol, leverage, quantity, max_quantity_ratio)
+    return {"message": "successful"}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    my_ip = requests.get('https://api.my-ip.io/ip').text
+    logger.info(f"My IP: {my_ip}")
+    uvicorn.run('main:app', host='0.0.0.0', port=8099)
---
+++ src/trading.py
@@ -0,0 +1,63 @@
+from binance import Client
+
+
+class BinanceLeverage:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+        self.leverage_mapping = {}
+
+    def change_leverage(self, symbol, leverage):
+        self.leverage_mapping[symbol] = leverage
+        self.client.futures_change_leverage(symbol=symbol, leverage=leverage)
+
+
+class BinanceBalance:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+
+    def get_max_qty(self, symbol, leverage):
+        account_balance = self.client.futures_account_balance()
+        ticker_price = self.client.futures_symbol_ticker(symbol=symbol)
+        mark_price = float(ticker_price['price'])
+        for asset in account_balance:
+            if asset['asset'] == symbol[-4:]:
+                balance = float(asset['balance'])
+        return balance * leverage / mark_price
+
+
+class BinanceOrder:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+        self.leverage = BinanceLeverage(api_key, api_secret)
+        self.balance = BinanceBalance(api_key, api_secret)
+
+    def get_precision(self, symbol):
+        info = self.client.futures_exchange_info()
+        for x in info['symbols']:
+            if x['symbol'] == symbol:
+                return x['quantityPrecision']
+
+    def create_order(self, side, symbol, leverage, quantity=None, max_quantity_ratio=0.1):
+        self.leverage.change_leverage(symbol, leverage)
+        if not quantity:
+            quantity = self.balance.get_max_qty(symbol, leverage) * max_quantity_ratio
+        precision = self.get_precision(symbol)
+        quantity = float(round(quantity, precision))
+        self.client.futures_create_order(
+            symbol=symbol,
+            type='MARKET',
+            side=side,
+            quantity=quantity
+        )
+
+
+class BinanceTrading:
+    def __init__(self, api_key, api_secret):
+        self.order = BinanceOrder(api_key, api_secret)
+
+    def buy(self, symbol, leverage, quantity=None, max_quantity_ratio=0.1):
+        self.order.create_order(Client.SIDE_BUY, symbol, leverage, quantity, max_quantity_ratio)
+
+    def sell(self, symbol, leverage, quantity=None, max_quantity_ratio=0.1):
+        self.order.create_order(Client.SIDE_SELL, symbol, leverage, quantity, max_quantity_ratio)
+
